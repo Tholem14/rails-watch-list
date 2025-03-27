62785ea849aff00cdd6d016775f451cb9c05621f
Rodrigo Alvarez de Toledo <alvarezdetoledo14@gmail.com>
2025-03-27 18:25:50 +0000

    ejercicio2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Borrando datos anteriores..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Creando películas..."
+movies = [
+  { title: "Inception", overview: "A mind-bending thriller about dream invasion.", poster_url: "https://image.tmdb.org/t/p/original/inception.jpg", rating: 8.8 },
+  { title: "The Dark Knight", overview: "Batman faces the Joker in Gotham City.", poster_url: "https://image.tmdb.org/t/p/original/dark_knight.jpg", rating: 9.0 },
+  { title: "Interstellar", overview: "A journey beyond our galaxy.", poster_url: "https://image.tmdb.org/t/p/original/interstellar.jpg", rating: 8.6 }
+]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
+
+puts "Películas creadas exitosamente."
